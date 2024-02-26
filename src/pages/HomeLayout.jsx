5b4bb9d24ae85c2a3008f3298a623637c4b6752d
Obisanya1 <ifeoluwadaniel25@gmail.com>
2024-02-26 15:50:28 +0100

    Created the Navbar.

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Navbar, Header } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
+    <Header/>
+    <Navbar/>
       <nav>
         <span className="text-4xl text-primary">Comfy</span>
       </nav>
